c4729fa2307e8b90eb4eb5b0682bfbbf6ef71da6
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-09-20 13:02:06 +0200

    Changed type to trapezoid

--- src/NodePlotter/Nodes.js
+++ src/NodePlotter/Nodes.js
@@ -41,16 +41,51 @@ export const TrustNode = memo(({ data }) => {
   );
 });
 
+// TODO: this is a parallelogram not a trapezoid
 export const SCorpNode = memo(({ data }) => {
   return (
-    <>
-      Trapezoid: SCorp Node
-      {data.label}
+    <div style={{ position: "relative" }}>
       <Handle type="source" position="top" id="a" />
       <Handle type="source" position="right" id="b" />
       <Handle type="source" position="bottom" id="c" />
       <Handle type="source" position="left" id="d" />
-    </>
+      <svg
+        width="150"
+        height="70"
+        style={{ display: "block", overflow: "visible" }}
+      >
+        <path
+          d="M0,70 L37.5,0 L150,0 L112.5,70 z"
+          fill="#668de3"
+          strokeWidth="2"
+          stroke="#fff"
+        ></path>
+      </svg>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div
+          style={{
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            color: "white",
+            fontSize: "12px",
+          }}
+        >
+          {data.label}
+        </div>
+      </div>
+    </div>
   );
 });
 
