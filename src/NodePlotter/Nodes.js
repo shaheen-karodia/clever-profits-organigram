d0c0db4114cacf4dfb4cdb6e48992be85cb8c9ca
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-09-20 22:29:21 +0200

    LLC Rendering Correctly

--- src/NodePlotter/Nodes.js
+++ src/NodePlotter/Nodes.js
@@ -1,6 +1,37 @@
 import React, { memo } from "react";
 import { Handle } from "react-flow-renderer";
 
+// ********* Helpers ******** //
+
+const svgStyleObj = { display: "block", overflow: "visible" };
+
+const DataLabel = ({ label }) => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+    }}
+  >
+    <div
+      style={{
+        fontFamily: "monospace",
+        fontWeight: "bold",
+        color: "white",
+        fontSize: "12px",
+      }}
+    >
+      {label}
+    </div>
+  </div>
+);
+
+// ************************************************** //
 export const PartnershipNode = memo(({ data }) => {
   return (
     <>
@@ -16,14 +47,22 @@ export const PartnershipNode = memo(({ data }) => {
 
 export const LLCNode = memo(({ data }) => {
   return (
-    <>
-      RECTANGLE: LLC
-      {data.label}
+    <div style={{ position: "relative" }}>
       <Handle type="source" position="top" id="a" />
       <Handle type="source" position="right" id="b" />
       <Handle type="source" position="bottom" id="c" />
       <Handle type="source" position="left" id="d" />
-    </>
+
+      <svg width="130" height="50" style={svgStyleObj}>
+        <path
+          d="M0,0 L120,0  L130,25 L120,50 L0,50 z"
+          fill="#784be8"
+          stroke-width="0"
+          stroke="#fff"
+        ></path>
+      </svg>
+      <DataLabel label={data.label} />
+    </div>
   );
 });
 
@@ -48,11 +87,7 @@ export const SCorpNode = memo(({ data }) => {
       <Handle type="source" position="right" id="b" />
       <Handle type="source" position="bottom" id="c" />
       <Handle type="source" position="left" id="d" />
-      <svg
-        width="150"
-        height="70"
-        style={{ display: "block", overflow: "visible" }}
-      >
+      <svg width="150" height="70" style={svgStyleObj}>
         <path
           d="M0,70 L37.5,0 L150,0 L112.5,70 z"
           fill="#668de3"
@@ -60,30 +95,7 @@ export const SCorpNode = memo(({ data }) => {
           stroke="#fff"
         ></path>
       </svg>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <div
-          style={{
-            fontFamily: "monospace",
-            fontWeight: "bold",
-            color: "white",
-            fontSize: "12px",
-          }}
-        >
-          {data.label}
-        </div>
-      </div>
+      <DataLabel label={data.label} />
     </div>
   );
 });
@@ -96,11 +108,7 @@ export const IndividualNode = memo(({ data }) => {
       <Handle type="source" position="bottom" id="c" />
       <Handle type="source" position="left" id="d" />
 
-      <svg
-        width="150"
-        height="50"
-        style={{ display: "block", overflow: "visible" }}
-      >
+      <svg width="150" height="50" style={svgStyleObj}>
         <ellipse
           cx="75"
           cy="25"
@@ -111,30 +119,7 @@ export const IndividualNode = memo(({ data }) => {
           stroke="#fff"
         ></ellipse>
       </svg>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <div
-          style={{
-            fontFamily: "monospace",
-            fontWeight: "bold",
-            color: "white",
-            fontSize: "12px",
-          }}
-        >
-          {data.label}
-        </div>
-      </div>
+      <DataLabel label={data.label} />
     </div>
   );
 });
