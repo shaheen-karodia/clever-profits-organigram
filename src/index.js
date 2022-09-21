9ad3dc06aa586630a5cd7238fa10de8fa0acae49
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-09-21 15:16:02 +0200

    added missing file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import MenuProvider from "react-flexible-sliding-menu";
 import "./styles.css";
 import App from "./App";
+import Menu from "./Menu";
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <MenuProvider MenuComponent={Menu} animation="push">
+      <App />
+    </MenuProvider>
   </StrictMode>,
   rootElement
 );
