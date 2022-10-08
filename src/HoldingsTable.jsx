b03b9d3841c15d986f0af566f3a8e06d51c76ca6
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-10-08 08:51:15 +0200

    move useHoldingState into own hook

--- src/HoldingsTable.jsx
+++ src/HoldingsTable.jsx
@@ -1,51 +1,10 @@
-import React, { useState } from "react";
-import { SkarTable, cellTypes, getAdditionalRow } from "./GeneralTableUtils";
+import React from "react";
+import { SkarTable } from "./GeneralTableUtils";
+import useHoldingsState from "./useHoldingsState";
 
-const HEADERS = ["Name", "Investment in", "Holdings %"];
-
-const ROW_SCHEMA = [
-  {
-    name: "name",
-    initialValue: "",
-    type: cellTypes.SELECT,
-    config: { placeholder: "Select an Entity", options: ["hello", "there"] },
-  },
-  {
-    name: "investmentIn",
-    initialValue: "",
-    type: cellTypes.SELECT,
-    config: { placeholder: "Select an Entity", options: ["replace", "me"] },
-  },
-  { name: "holdings", initialValue: "", type: cellTypes.INPUT },
-];
-
-const getAdditionalHoldingRow = getAdditionalRow(ROW_SCHEMA);
 function HoldingsTable() {
-  const [holdings, setHoldings] = useState([
-    getAdditionalHoldingRow(),
-    getAdditionalHoldingRow(),
-  ]);
-
-  const onCellChange = ({ rowId, property, value }) => {
-    const newHoldings = holdings.map((h) => {
-      if (h.id === rowId) {
-        h[property] = value;
-      }
-      return h;
-    });
-
-    setHoldings(newHoldings);
-  };
-
-  const onRowDelete = (holding) => {
-    const updatedHoldings = holdings.filter((h) => h.id !== holding.id);
-    setHoldings(updatedHoldings);
-  };
-
-  const onRowAdd = () => {
-    setHoldings([...holdings, getAdditionalHoldingRow()]);
-  };
-
+  const { onCellChange, onRowDelete, onRowAdd, holdings, ROW_SCHEMA, HEADERS } =
+    useHoldingsState();
   return (
     <div>
       <h2>Holdings Table</h2>
---
+++ src/useHoldingsState.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { cellTypes, getAdditionalRow } from "./GeneralTableUtils";
+
+const HEADERS = ["Name", "Investment in", "Holdings %"];
+
+const ROW_SCHEMA = [
+  {
+    name: "name",
+    initialValue: "",
+    type: cellTypes.SELECT,
+    config: { placeholder: "Select an Entity", options: ["hello", "there"] }, //TODO how do we make these conditional
+  },
+  {
+    name: "investmentIn",
+    initialValue: "",
+    type: cellTypes.SELECT,
+    config: { placeholder: "Select an Entity", options: ["replace", "me"] }, //TODO how do we make these conditional
+  },
+  { name: "holdings", initialValue: "", type: cellTypes.INPUT },
+];
+
+const getAdditionalHoldingRow = getAdditionalRow(ROW_SCHEMA);
+
+function useHoldingsState() {
+  const [holdings, setHoldings] = useState([
+    getAdditionalHoldingRow(),
+    getAdditionalHoldingRow(),
+    getAdditionalHoldingRow(),
+    getAdditionalHoldingRow(),
+  ]);
+
+  const onCellChange = ({ rowId, property, value }) => {
+    const newHoldings = holdings.map((h) => {
+      if (h.id === rowId) {
+        h[property] = value;
+      }
+      return h;
+    });
+
+    setHoldings(newHoldings);
+  };
+
+  const onRowDelete = (holding) => {
+    const updatedHoldings = holdings.filter((h) => h.id !== holding.id);
+    setHoldings(updatedHoldings);
+  };
+
+  const onRowAdd = () => {
+    setHoldings([...holdings, getAdditionalHoldingRow()]);
+  };
+
+  return { onCellChange, onRowDelete, onRowAdd, holdings, ROW_SCHEMA, HEADERS };
+}
+
+export default useHoldingsState;
